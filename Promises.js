1f7870def64e7958ea6806ad1562d761bffb1390
Daniel Ontiveros <cardanont@gmail.com>
2019-08-21 23:54:39 -0500

    The promise is true

--- Promises.js
+++ Promises.js
@@ -1,20 +1,9 @@
-// Before Promises Callbacks
+// Promise
 
-el.addEventListener("click", submitForm);
-
-// callback pyramid of doom
-movePlayer(100, 'Left', function(){
-    movePlayer(400, 'Left', function(){
-        movePlayer(10, 'Right', function(){
-            movePlayer(330, 'Left', function(){
-            });
-        });
-    });
-});
-
-// With promise 
-
-movePlayer(100, 'Left')
-    .then(() => movePlayer(400, 'Left'))
-    .then(() => movePlayer(10, 'Right'))
-    .then(() => movePlayer(330, 'Left'))
+const promise = new Promise((resolve, reject) => {
+    if(true){
+        resolve('Stuff Sorked');
+    }else{
+        reject('Error, it broke');
+    }
+})
