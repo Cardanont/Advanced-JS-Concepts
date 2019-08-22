39c519246af487fedd2fdc64d700fc737b7b73e6
Daniel Ontiveros <cardanont@gmail.com>
2019-08-22 00:02:07 -0500

    Testing promises

--- Promises.js
+++ Promises.js
@@ -2,8 +2,15 @@
 
 const promise = new Promise((resolve, reject) => {
     if(true){
-        resolve('Stuff Sorked');
+        resolve('Stuff Worked');
     }else{
         reject('Error, it broke');
     }
 })
+
+promise.then(result => result + '!')
+    .then(result2 => result2 + '?')
+    .catch(() => console.log('error!'))
+    .then(result3 => {
+        console.log(result3 + '!');
+    });
